09a0b5b62c7fce35a6ec9c39c49b0c43f2523cdb
Liam Swetnam <lvswetnam@gmail.com>
2025-06-05 19:19:40 -0500

    feat(frontend): Update middleware for admin panel usage

--- tiqet-frontend/src/middleware.ts
+++ tiqet-frontend/src/middleware.ts
@@ -25,6 +25,36 @@ export default async function middleware(request: NextRequest) {
     }
   }
 
+  if (request.nextUrl.pathname === '/admin') {
+    if (token) {
+      const cookie = request.headers.get('cookie')!
+      const userResp = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user`, {
+        headers: {
+          cookie,
+        },
+      })
+
+      if (userResp.status === 200) {
+        const userData = await userResp.json()
+        const userRoles = userData.data.roles as string[]
+        if (userRoles.includes('Admin')) {
+          return NextResponse.next()
+        }
+
+        return NextResponse.redirect(new URL('/', request.url))
+      }
+
+      const invalidTokenResp = NextResponse.redirect(new URL('/', request.url))
+      invalidTokenResp.cookies.set('token', '', {
+        expires: new Date(0),
+        path: '/',
+      })
+      return invalidTokenResp
+    }
+
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
   if (![
     '/login',
     '/favicon.ico',
