316013b222b347049f2c54afe2b03a9613e33e48
Liam Swetnam <lvswetnam@gmail.com>
2025-06-08 13:46:13 -0500

    chore(backend): Remove unused `GetUserByIDEndpoint` function

--- tiqet-backend/pkg/api/endpoints/user.go
+++ tiqet-backend/pkg/api/endpoints/user.go
@@ -2,7 +2,6 @@ package endpoints
 
 import (
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lvsweat/Tiqet/tiqet-backend/pkg/utils"
@@ -34,29 +33,5 @@ func GetUserEndpoint(c *gin.Context) {
 	case 2:
 		c.IndentedJSON(http.StatusUnauthorized, gin.H{"data": nil})
 		return
-	}
-}
-
-func GetUserByIDEndpoint(c *gin.Context) {
-	strId := c.Param("id")
-
-	id, idErr := strconv.Atoi(strId)
-	if idErr != nil {
-		c.IndentedJSON(http.StatusInternalServerError, gin.H{"data": nil})
-		return
-	}
-
-	user, err := utils.GetUserByID(uint32(id))
-
-	if err == 0 {
-		c.IndentedJSON(http.StatusOK, gin.H{"data": user})
-		return
-	}
-
-	switch err {
-	case 1:
-		c.IndentedJSON(http.StatusNotFound, gin.H{"data": nil})
-	case 2:
-		c.IndentedJSON(http.StatusInternalServerError, gin.H{"data": nil})
 	}
 }
